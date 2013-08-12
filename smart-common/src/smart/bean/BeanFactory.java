0a503e67e460b6564594398f281c080e2695ce7d
Jiangwei Xu <hermit86@163.com>
2013-08-12 10:08:21 +0800

    修改 CPU 实体

---
+++ smart-common/src/smart/bean/BeanFactory.java
@@ -0,0 +1,29 @@
+package smart.bean;
+
+import smart.bean.host.Host;
+
+/**
+ * Bean 工厂。
+ * 
+ * @author Jiangwei Xu
+ */
+public final class BeanFactory {
+
+	private static final BeanFactory instance = new BeanFactory();
+
+	private BeanFactory() {
+	}
+
+	public static BeanFactory getInstance() {
+		return BeanFactory.instance;
+	}
+
+	/**
+	 * 创建主机实体。
+	 * @param id
+	 * @return
+	 */
+	public Host createHost(String id) {
+		return new Host(id);
+	}
+}
--- smart-common/src/smart/bean/host/CPU.java
+++ smart-common/src/smart/bean/host/CPU.java
@@ -1,6 +1,6 @@
 package smart.bean.host;
 
-import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import smart.entity.AbstractEntity;
 
@@ -12,96 +12,55 @@ public class CPU extends AbstractEntity {
 	private static final long serialVersionUID = -3010764141583766821L;
 
 	private String vendor;
-	private String type;
-	private float clockSpeed;
-	private int cacheSize;
-	private double user;
-	private double nice;
-	private double system;
-	private double IOWait;
-	private double idle;
-	private double cpuLoad;
-	private double usage;
-	private List<CPUUsage> cpuUsage;
-	
+	private String model;
+	private int clockSpeed;
+	private long cacheSize;
+	private int totalCores;
+
+	/// CPU 利用率（百分比），Key: 数据时间戳
+	private ConcurrentHashMap<Long, CPUPerc> percs;
 
 	public CPU(String id) {
 		super(id);
+		this.percs = new ConcurrentHashMap<Long, CPUPerc>(2);
 	}
 
 	public String getVendor() {
-		return vendor;
+		return this.vendor;
 	}
 	public void setVendor(String vendor) {
 		this.vendor = vendor;
 	}
-	public String getType() {
-		return type;
+
+	public String getModel() {
+		return this.model;
 	}
-	public void setType(String type) {
-		this.type = type;
+	public void setModel(String model) {
+		this.model = model;
 	}
-	public float getClockSpeed() {
-		return clockSpeed;
+
+	public int getMhz() {
+		return this.clockSpeed;
 	}
-	public void setClockSpeed(float clockSpeed) {
-		this.clockSpeed = clockSpeed;
+	public void setMhz(int mhz) {
+		this.clockSpeed = mhz;
 	}
-	public int getCacheSize() {
-		return cacheSize;
+
+	public long getCacheSize() {
+		return this.cacheSize;
 	}
-	public void setCacheSize(int cacheSize) {
+	public void getCacheSize(long cacheSize) {
 		this.cacheSize = cacheSize;
 	}
-	public double getUser() {
-		return user;
+
+	public int getTotalCores() {
+		return this.totalCores;
 	}
-	public void setUser(double user) {
-		this.user = user;
-	}
-	public double getNice() {
-		return nice;
-	}
-	public void setNice(double nice) {
-		this.nice = nice;
-	}
-	public double getSystem() {
-		return system;
-	}
-	public void setSystem(double system) {
-		this.system = system;
-	}
-	public double getIOWait() {
-		return IOWait;
-	}
-	public void setIOWait(double iOWait) {
-		IOWait = iOWait;
-	}
-	public double getIdle() {
-		return idle;
-	}
-	public void setIdle(double idle) {
-		this.idle = idle;
-	}
-	public double getCpuLoad() {
-		return cpuLoad;
-	}
-	public void setCpuLoad(double cpuLoad) {
-		this.cpuLoad = cpuLoad;
-	}
-	public double getUsage() {
-		return usage;
-	}
-	public void setUsage(double usage) {
-		this.usage = usage;
+	public void setTotalCores(int totalCores) {
+		this.totalCores = totalCores;
 	}
 
-	public List<CPUUsage> getCpuUsage() {
-		return cpuUsage;
+	public void addPrec(long timestamp, CPUPerc perc) {
+		this.percs.put(timestamp, perc);
 	}
-
-	public void setCpuUsage(List<CPUUsage> cpuUsage) {
-		this.cpuUsage = cpuUsage;
-	}
-	
 }
