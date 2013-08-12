784b96605f1e235eead008f122ad2c14ee0f2d7b
Jiangwei Xu <hermit86@163.com>
2013-08-12 10:10:04 +0800

    新增文件

---
+++ smart-common/src/smart/bean/host/CPUPerc.java
@@ -0,0 +1,20 @@
+package smart.bean.host;
+
+import java.io.Serializable;
+
+/**
+ * 
+ */
+public final class CPUPerc implements Serializable {
+
+	private static final long serialVersionUID = 1335146206472829631L;
+
+	private double user;
+
+	public CPUPerc() {
+	}
+
+	public double getUser() {
+		return this.user;
+	}
+}
