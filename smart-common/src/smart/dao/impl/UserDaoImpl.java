b9fca379d4b0b4f7c179534dbd90c74711184ad6
yanbo <yanbo@dhcc.com.cn>
2013-09-04 17:36:25 +0800

    修改用户Dao层方法及用户实体属性。

--- smart-common/src/smart/dao/impl/UserDaoImpl.java
+++ smart-common/src/smart/dao/impl/UserDaoImpl.java
@@ -2,12 +2,12 @@ package smart.dao.impl;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 
 import smart.bean.User;
 import smart.dao.AbstraceDao;
 import smart.dao.UserDao;
+import smart.util.DButil;
 
 /**
  * 用户 DAO 实现。
@@ -16,56 +16,14 @@ public class UserDaoImpl extends AbstraceDao implements UserDao {
 
 	private PreparedStatement pstmt;
 	private ResultSet rs;
-	
+
 	public UserDaoImpl() {
 		super();
 	}
 
 	@Override
-	public List<User> getUSers() {
-		String sql = "select * from onlineUser";
-		List<User> list = new ArrayList<User>(20);
-		try {
-			super.doStart();
-			pstmt = super.conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-			while (rs.next()) {
-				User user = new User(rs.getLong("id"));
-				user.setName(rs.getString("name"));
-				user.setPwd(rs.getString("password"));
-				user.setIp(rs.getString("ip"));
-				user.setToken(rs.getString("token"));
-				user.setPort(rs.getInt("port"));
-				list.add(user);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return list;
-	}
-
-	@Override
-	public String getName(String token, String ip) {
-		String sql = "select name from onlineUser where token = ? and ip = ?";
-		String name = null;
-		try {
-			super.doStart();
-			pstmt = super.conn.prepareStatement(sql);
-			pstmt.setString(1, token);
-			pstmt.setString(2, ip);
-			rs = pstmt.executeQuery();
-			if (rs.next()) {
-				name = rs.getString("name");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return name;
-	}
-
-	@Override
 	public String getName(String token) {
-		String sql = "select name from onlineUser where token = ?";
+		String sql = "select name from t_user where token = ?";
 		String name = null;
 		try {
 			super.doStart();
@@ -82,23 +40,21 @@ public class UserDaoImpl extends AbstraceDao implements UserDao {
 	}
 
 	@Override
-	public User getUser(String username, String password, String ip) {
-		String sql = "select * form onlineUser where username = ? and password = ? and ip = ?";
+	public User getUser(String username, String password) {
+		String sql = "select * from t_user where name = ? and password = ?";
 		User user = null;
 		try {
 			super.doStart();
 			pstmt = super.conn.prepareStatement(sql);
 			pstmt.setString(1, username);
 			pstmt.setString(2, password);
-			pstmt.setString(3, ip);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
 				user = new User(rs.getLong("id"));
 				user.setName(rs.getString("name"));
 				user.setPwd(rs.getString("password"));
 				user.setToken(rs.getString("token"));
-				user.setIp(rs.getString("ip"));
-				user.setPort(rs.getInt("port"));
+				user.setLastLogin(rs.getLong("lastLogin"));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -107,27 +63,61 @@ public class UserDaoImpl extends AbstraceDao implements UserDao {
 	}
 
 	@Override
-	public User getUser(String token, String ip) {
-		String sql = "select * form onlineUser where token = ? and ip = ?";
+	public User getUser(String token) {
+		String sql = "select * form t_user where token = ?";
 		User user = null;
 		try {
 			super.doStart();
 			pstmt = super.conn.prepareStatement(sql);
 			pstmt.setString(1, token);
-			pstmt.setString(2, ip);
 			rs = pstmt.executeQuery();
 			if (rs.next()) {
 				user = new User(rs.getLong("id"));
 				user.setName(rs.getString("name"));
 				user.setPwd(rs.getString("password"));
 				user.setToken(rs.getString("token"));
-				user.setIp(rs.getString("ip"));
-				user.setPort(rs.getInt("port"));
+				user.setLastLogin(rs.getLong("lastLogin"));
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return user;
+	}
+
+	@Override
+	public void saveUser(long id, String name, String passwordMD5,
+			String token) {
+		String sql = "insert into t_user (id, name, password, token, lastLogin) values (?,?,?,?,?)";
+		try {
+			super.doStart();
+			pstmt = super.conn.prepareStatement(sql);
+			pstmt.setLong(1, id);
+			pstmt.setString(2, name);
+			pstmt.setString(3, passwordMD5);
+			pstmt.setString(4, token);
+			pstmt.setLong(5, (new Date()).getTime());
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DButil.close(pstmt, null);
+		}
+	}
+
+	@Override
+	public void updateUser(String name) {
+		String sql = "update t_user set lastLogin = ? where name = ?";
+		try {
+			super.doStart();
+			pstmt = super.conn.prepareStatement(sql);
+			pstmt.setLong(1, (new Date()).getTime());
+			pstmt.setString(2, name);
+			pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			DButil.close(pstmt, null);
+		}
 	}
 
 }
