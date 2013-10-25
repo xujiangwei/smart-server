13e7f50384dd9baeb7938ef9a0c468de922cd8d2
qw1210p <qw1210p@gmail.com>
2013-10-25 17:18:33 +0800

    增加了接口监听

---
+++ smart-cellet/src/smart/action/task/IncidentRelatedCiListener.java
@@ -0,0 +1,126 @@
+package smart.action.task;
+
+import java.nio.charset.Charset;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import net.cellcloud.common.Logger;
+import net.cellcloud.core.Cellet;
+import net.cellcloud.talk.dialect.ActionDialect;
+import net.cellcloud.util.ObjectProperty;
+import net.cellcloud.util.Properties;
+
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.http.HttpMethod;
+import org.eclipse.jetty.http.HttpStatus;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import smart.action.AbstractListener;
+import smart.api.API;
+import smart.api.host.HostConfig;
+import smart.api.host.HostConfigContext;
+import smart.api.host.ServiceDeskHostConfig;
+import smart.mast.action.Action;
+
+
+/** 
+* @Description :  故障工单处理并保存操作
+* @author      :  dwg
+* @date        :  Oct 22, 2013 11:07:11 AM 
+*  
+*/
+public final class IncidentRelatedCiListener extends AbstractListener {
+
+
+	public IncidentRelatedCiListener(Cellet cellet) {
+		super(cellet);
+	}
+
+	@Override
+	public void onAction(ActionDialect action) {
+
+		// 使用同步方式进行请求。
+		// 因为 onAction 方法是由 Cell Cloud 的 action dialect 进行回调的，
+		// 该方法独享一个线程，因此可以在此线程里进行阻塞式的调用。
+		// 因此，可以用同步方式请求 HTTP API 。
+
+		// URL
+		HostConfig  serviceDeskConfig=new ServiceDeskHostConfig();
+		HostConfigContext context=new HostConfigContext(serviceDeskConfig);
+		StringBuilder url = new StringBuilder(context.getAPIHost()).append("/").append(API.INCIDENTRELATEDCIS);
+		JSONObject json = null;
+		String bpiId=null;
+		try {
+			json = new JSONObject(action.getParamAsString("data"));
+			bpiId=json.getString("incidentId");
+			
+		} catch (JSONException e2) {
+			e2.printStackTrace();
+		}
+		
+		url.append("&bpiId=").append(bpiId);
+			// 创建请求
+		Request request = this.getHttpClient().newRequest(url.toString());
+		request.method(HttpMethod.GET);
+		
+		Properties params = new Properties();
+		// 发送请求
+		ContentResponse response = null;
+		try {
+			response = request.send();
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		} catch (TimeoutException e1) {
+			e1.printStackTrace();
+		} catch (ExecutionException e1) {
+			e1.printStackTrace();
+		}
+		
+		JSONObject jo = null;
+		switch (response.getStatus()) {
+		case HttpStatus.OK_200:
+			byte[] bytes = response.getContent();
+		
+			if (null != bytes) {
+				// 获取从 Web 服务器上返回的数据
+				String content = new String(bytes, Charset.forName("UTF-8"));
+				try {
+					jo = new JSONObject(content);
+					System.out.println("响应工单保存数据的返回值为：:" + jo);
+
+					// 设置参数
+					params.addProperty(new ObjectProperty("data", jo));
+
+					// 响应动作，即向客户端发送 ActionDialect
+					this.response(Action.INCIDENTRELATEDCIS, params);
+
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			} else {
+				this.reportHTTPError(Action.INCIDENTRELATEDCIS);
+			}
+
+			break;
+		default:
+			Logger.w(IncidentListListener.class,
+					"返回响应码：" + response.getStatus());
+			jo = new JSONObject();
+			try {
+				jo.put("status", 900);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			// 设置参数
+			params.addProperty(new ObjectProperty("data", jo));
+
+			// 响应动作，即向客户端发送 ActionDialect
+			this.response(Action.INCIDENTRELATEDCIS, params);
+			break;
+		}
+	}
+
+}
